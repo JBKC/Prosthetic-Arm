ed8ff4ab23e63baa08c218c60881d84335724a43
James Bannon <121674801+JBKC@users.noreply.github.com>
2024-08-13 06:57:21 +0100

    blender script to be used natively

--- blender_accelerometer_test.py
+++ blender_accelerometer_test.py
@@ -1,36 +1,48 @@
 import bpy
-import math
+import serial
+import threading
 
+# set up streaming from arduino
+arduino_port = '/dev/tty.usbmodem14201'
+baud_rate = 115200
+ser = serial.Serial(arduino_port, baud_rate)
 
-def update_arm():
-    obj = bpy.data.objects.get('Cube')
+# create queue for Arduino data
+data_queue = []
 
-    if obj:
-        obj.rotation_euler = (math.radians(20), math.radians(30), math.radians(60))
-        bpy.context.view_layer.update()
-        print('Object updated')
-    else:
-        print('Object not found')
+def serial_thread():
+    global data_queue
+    while True:
+        # check if there is incoming data to read
+        if ser.in_waiting > 0:
+            # read from serial
+            data = ser.readline().decode('utf-8').strip().split(',')
+            # if serial expected size, then add to queue
+            if len(data) == 3:
+                data_queue = list(map(float, data))
 
-
-class SimpleOperator(bpy.types.Operator):
-    bl_idname = "object.simple_operator"
-    bl_label = "Simple Object Operator"
-
-    def execute(self, context):
-        update_arm()
-        return {'FINISHED'}
-
+def update_blender():
+    # extract data from queue
+    if data_queue:
+        x, y, z = data_queue
+        # update object
+        obj = bpy.data.objects.get('Cube')
+        if obj:
+            obj.rotation_euler = (x, y, z)
+            bpy.context.view_layer.update()
+            print(f"Updated Cube rotation to: {x}, {y}, {z}")
+    # check for updates every 0.5s
+    return 0.5
 
 def register():
-    bpy.utils.register_class(SimpleOperator)
-
+    # create thread that runs serial_thread function
+    threading.Thread(target=serial_thread, daemon=True).start()
+    # concurrently update object
+    bpy.app.timers.register(update_blender)
 
 def unregister():
-    bpy.utils.unregister_class(SimpleOperator)
-
+    bpy.app.timers.unregister(update_blender)
+    ser.close()
 
 if __name__ == "__main__":
     register()
-    # Test call
-    bpy.ops.object.simple_operator()
